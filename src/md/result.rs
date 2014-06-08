ba0f87a461e33e3f9a59afe57929a276dc0108a8
Vladimir Matveev <vladimir.matweev@gmail.com>
2014-06-08 15:17:24 +0400

    Added missing result module

---
+++ src/md/result.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::fmt;
+
+pub enum PartialResult<T, E1, E2> {
+    Success(T),
+    PartialSuccess(T, E1),
+    Failure(E2)
+}
+
+impl<T, E1, E2> PartialResult<T, E1, E2> {
+    pub fn to_result(self) -> Result<T, E2> {
+        match self {
+            Success(value) | PartialSuccess(value, _) => Ok(value),
+            Failure(e) => Err(e)
+        }
+    }
+}
+
+pub type MarkdownResult<T> = PartialResult<T, MarkdownError, MarkdownError>;
+
+pub enum MarkdownError {
+    ParseError(&'static str),
+    IoError(io::IoError)
+}
+
+impl fmt::Show for MarkdownError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ParseError(ref msg) => write!(f, "parse error: {}", msg),
+            IoError(ref err) => write!(f, "i/o error: {}", err)
+        }
+    }
+}
+
+impl MarkdownError {
+    #[inline]
+    pub fn from_io(err: io::IoError) -> MarkdownError {
+         IoError(err)
+    }
+}
