67fede1ac753e233665f6d6f29bf6db59ed34cba
Vladimir <netvl@qplex.local>
2014-08-12 15:15:53 +0400

    More on parsing inlines

--- src/lib.rs
+++ src/lib.rs
@@ -277,8 +277,19 @@ macro_rules! iotry_err(
 macro_rules! break_err(
     ($e:expr; -> $err:ident) => (
         match $e {
-            Failure(e) => { $err = Some(e); break }
-            PartialSuccess(_, e) => { $err = Some(e); break }
+            Failure(e) | PartialSuccess(_, e) => { 
+                $err = Some(e); 
+                break
+            }
+            o => o
+        }
+    );
+    ($e:expr; -> $err:ident; $lt:ident) => (
+        match $e {
+            Failure(e) | PartialSuccess(_, e) => { 
+                $err = Some(e); 
+                break $lt
+            }
             o => o
         }
     )
@@ -826,29 +837,75 @@ impl<R: Reader> MarkdownParser<R> {
 
         fn find_emph_closing<R: Reader>(this: MarkdownParser<R>, ec: u8, n: uint) 
             -> ParserResult<Vec<u8>> {
+            assert!(n > 0);  // need at least one emphasis character
 
             let mut result = Vec::new();
             let mut err = None;
+            let mut escaping = false;
 
             'outer: loop {
                 match break_err!(this.read_byte_pr(); -> err).unwrap() {
+                    // pass escaped characters as is
+                    b'\\' => { result.push(b'\\'); escaping = true }
+                    c if escaping => { result.push(c); escaping = false }
+
+                    // found our emphasis character
                     c if c == ec => {
-                        let mut n = n;
-                        while n > 1 {
-                            match this.read_char(ec) {
-                                Success(_) => n += 1,
-                                NoParse => { result.push(c); continue 'outer }
-                                Failure(e) | PartialSuccess(_, e) => {
-                                    err = Some(e);
+                        let mut rn = 1;
+                        this.mark().set();
+                        while break_err!(this.read_char(ec); -> err).is_success() {
+                            rn += 1;
+                        }
+
+                        if rn == n { // this is our emphasis boundary, finish reading
+                            this.unread_byte();
+                            this.mark().unset();
+                            break;
+                            
+                        } else { // otherwise reset to the character just after the one we just have read
+                            this.mark().reset();
+                            result.push(c);
+                        }
+                    }
+
+                    // inline code block started, and we're not inline code block ourselves
+                    // we need to pass through this block as is
+                    c if c == b'`' => {  
+                        // count `s
+                        let mut sn = 1;
+                        while break_err!(this.read_char(b'`'); -> err).is_success() {
+                            result.push(b'`');
+                            sn += 1;
+                        }
+                        this.unread_byte();
+
+                        let mut tn = 0;
+                        while tn < sn {
+                            match this.read_byte() {
+                                Ok(b) => {
+                                    result.push(b);
+                                    match b {
+                                        b'`' => tn += 1,
+                                        _    => tn = 0;
+                                    }
+                                },
+
+                                Err(e) => {
+                                    err = Some(IoError(e));
                                     break 'outer;
                                 }
                             }
                         }
-
-
                     }
+
+                    // TODO: skip hyperlinks
+
+                    // just skip any other character
+                    c => result.push(c)
                 }
             }
+
+            Success(result)
         }
 
         loop {
@@ -876,6 +933,7 @@ impl<R: Reader> MarkdownParser<R> {
                         n += 1;
                     }
 
+                    // read everything until closing emphasis bracket
                     let buf = break_err!(find_emph_closing(self, c, n); -> err).unwrap();
                     self.push_existing(buf);
                     let result = break_err!(self.inline(); -> err).unwrap();
